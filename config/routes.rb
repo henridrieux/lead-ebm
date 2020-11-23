dd3abb683f003cbe71b13d066fef07c5ba5dab67
Tristan de Lattre <delattretristan@gmail.com>
2020-11-23 18:12:52 +0100

    routes in da place

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources companies:, only: [:index, :show]
+resources recruitments:, only: [:index, :show]
+resources categories:, only: [:index, :show]
+resources events:, only: [:index, :show]
+resources events_categories: only: [:index, :show] do
+  resources subscriptions:
+end
+
+
 end
