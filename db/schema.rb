bab1d751a5e3ab300ee3bdc778f75e8115d667ca
Pierre David <pierreaymeric.david@gmail.com>
2020-11-25 16:51:12 +0100

    clean home

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_152632) do
+ActiveRecord::Schema.define(version: 2020_11_25_141647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,10 @@ ActiveRecord::Schema.define(version: 2020_11_24_152632) do
     t.date "publication_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "employer_email"
+    t.text "job_description"
+    t.string "employer_phone"
+    t.string "employer_name"
     t.index ["category_id"], name: "index_recruitments_on_category_id"
   end
 
