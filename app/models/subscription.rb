33924fbf18bde5e6c02e4769a5f05298d9131c06
Tristan de Lattre <delattretristan@gmail.com>
2020-11-26 16:15:36 +0100

    value of subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,5 @@ class Subscription < ApplicationRecord
   belongs_to :user
   has_one :event, through: :event_category
   has_one :category, through: :event_category
+  validates :status, inclusion: { in: ["Activé", "Désactivé", "En Suspens"] }
 end
