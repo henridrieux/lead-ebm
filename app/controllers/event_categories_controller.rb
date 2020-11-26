60fb7ef78fe03a018233a14e24ad59399123379a
Tristan de Lattre <delattretristan@gmail.com>
2020-11-26 11:47:01 +0100

    policy and show

--- app/controllers/event_categories_controller.rb
+++ app/controllers/event_categories_controller.rb
@@ -1,9 +1,11 @@
 class EventCategoriesController < ApplicationController
+  after_action :verify_authorized, except: [:show, :index], unless: :skip_pundit?
   def index
-    @event_categories = EventCategory.all
+    @event_categories = policy_scope(EventCategory.all)
   end
 
   def show
     @event_categories = EventCategory.all
+    @category = Category.find(params[:id])
   end
 end
