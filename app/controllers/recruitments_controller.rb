38f3737705429c29f0e8b24774bce7e5bd2ee6d4
Tristan de Lattre <delattretristan@gmail.com>
2020-11-26 16:07:57 +0100

    recruitment change

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,9 +1,11 @@
 class RecruitmentsController < ApplicationController
+  after_action :verify_authorized, except: [:show, :index], unless: :skip_pundit?
+
   def index
-    @recruitments = Recruitment.All
+    @recruitments = policy_scope(Recruitment.all)
   end
 
   def show
-    @recruitment = Recruitment.find(params[:id])
+    @recruitment = policy_scope(Recruitment.find(params[:id]))
   end
 end
--- app/models/event_category.rb
+++ app/models/event_category.rb
@@ -1,6 +1,7 @@
 class EventCategory < ApplicationRecord
   belongs_to :category
   belongs_to :event
+  has_many :recruitments, through: :categories
   validates :title, presence: true
 end
 
