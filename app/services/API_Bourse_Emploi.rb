bea2d6ab8db3fdd34925784692853def1200d279
Chafik Argoub <cargoub@gmail.com>
2020-11-30 18:37:12 +0100

    Refactor API Bourse et Papers

--- app/services/API_Bourse_Emploi.rb
+++ app/services/API_Bourse_Emploi.rb
@@ -1,22 +1,47 @@
 class APIBourseEmploi
-# Or wrap things up in your own class
+require 'HTTParty'
 require "json"
-require "open-uri"
-require "net/http"
-
   def bourse_emploi
-    url = URI("https://bourse-emplois.notaires.fr/api/offre/search?page=1&pageSize=90&sort=DESC&sortField=DATE_ACTUALISATION")
-    https = Net::HTTP.new(url.host, url.port)
-    https.use_ssl = true
-    request = Net::HTTP::Post.new(url)
-    request["Pragma"] = "no-cache"
-    request["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36"
-    request["Content-Type"] = "application/json"
-    request["Accept"] = "*/*"
-    request["Cookie"] = "_ga=GA1.3.1335572239.1606137983; _gid=GA1.3.89168093.1606137983; tartaucitron=!analytics=true!recaptcha=true; Cookie_1=value"
-    request.body = "{\"cityFilter\":[],\"departmentFilter\":[],\"functionFilter\":[],\"geolocalisationFilter\":{},\"hasNextPage\":false,\"otherFilter\":{\"workingTime\":[],\"type\":[],\"professionalFormation\":[],\"availability\":[],\"keyWord\":\"\"},\"pageNumber\":1,\"pageSize\":90,\"regionFilter\":[],\"resultCount\":0,\"sortMethod\":\"DESC\",\"launchSearch\":false,\"sortField\":\"DATE_ACTUALISATION\"}"
-    response = https.request(request)
-    return_array = response.read_body
+    url = "https://bourse-emplois.notaires.fr/api/offre/search"
+    body_request = {
+      cityFilter: [],
+      departmentFilter: [],
+      functionFilter: [],
+      geolocalisationFilter: {},
+      hasNextPage: false,
+      otherFilter: {
+        workingTime: [],
+        type: [],
+        professionalFormation: [],
+        availability: [],
+        keyWord: ""
+      },
+      pageNumber:1,
+      pageSize:90,
+      regionFilter: [],
+      resultCount: 0,
+      sortMethod: "DESC",
+      launchSearch: false,
+      sortField: "DATE_ACTUALISATION"
+    }
+    @options = {
+      query: {
+        page: 1,
+        pageSize: 90,
+        sort: "DESC",
+        sortField: "DATE_ACTUALISATION"
+      },
+      headers: {
+        pragma: "no-cache",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+        "Content-Type": "application/json",
+        accept: "*/*",
+        cookie: "_ga=GA1.3.1364850714.1606138104; _gid=GA1.3.828954730.1606138104; tartaucitron=!analytics=true!recaptcha=true"
+      },
+      body: body_request.to_json
+    }
+    response = HTTParty.post(url, @options)
+    return_array = response.body
     result = JSON.parse(return_array)
     new_id_array = []
     result["content"].each do |v|
@@ -28,17 +53,28 @@ require "net/http"
       final_array << recruit
     end
     return final_array
+
   end
 
   def transform_json(id)
-    url2 = URI("https://bourse-emplois.notaires.fr/api/offre/preview/#{id}")
-    https = Net::HTTP.new(url2.host, url2.port)
-    https.use_ssl = true
-    request = Net::HTTP::Get.new(url2)
-    request["Cookie"] = "_ga=GA1.3.1335572239.1606137983; _gid=GA1.3.89168093.1606137983; tartaucitron=!analytics=true!recaptcha=true; Cookie_1=value"
-    response2 = https.request(request)
-    return_array2 = response2.read_body
+    url2 = "https://bourse-emplois.notaires.fr/api/offre/preview/#{id}"
+    body_request = {
+    }
+    @options = {
+      query: {
+      },
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+        "Content-Type": "application/json",
+        accept: "*/*",
+        cookie: "_ga=GA1.3.1335572239.1606137983; _gid=GA1.3.89168093.1606137983; tartaucitron=!analytics=true!recaptcha=true; Cookie_1=value"
+      },
+      body: body_request.to_json
+    }
+    response2 = HTTParty.get(url2, @options)
+    return_array2 = response2.body
     result2 = JSON.parse(return_array2)
     return result2
   end
 end
+
