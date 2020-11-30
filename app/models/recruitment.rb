cdaa76a0799e868d80a9bc2d4f80a916074af616
Henri des Rieux <desrieux.henri@gmail.com>
2020-11-30 15:11:03 +0100

    textslack

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -6,4 +6,12 @@ class Recruitment < ApplicationRecord
   # validates :contract_type, presence: true
   # validates :publication_date, presence: true
   validates :external_id, presence: true, uniqueness: true
+
+  def slack
+  POST https://hooks.slack.com/services/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
+  Content-type: application/json
+  {
+     "text": "Hello, world."
+  }
+  end
 end
