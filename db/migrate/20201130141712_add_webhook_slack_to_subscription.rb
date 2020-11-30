d9daa096a92f685765d6f2608879b785974c91c5
Pierre David <pierreaymeric.david@gmail.com>
2020-11-30 16:10:42 +0100

    webhook migration and json generation in eventcatmodel

--- db/migrate/20201130141712_add_webhook_slack_to_subscription.rb
+++
@@ -1,5 +0,0 @@
-class AddWebhookSlackToSubscription < ActiveRecord::Migration[6.0]
-  def change
-    add_column :subscriptions, :webhook_slack, :text, default: "https://hooks.slack.com/services/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"
-  end
-end
