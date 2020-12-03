b1f3c72f90530e4d6d06aeea17e00b13d3028a12
Pierre David <pierreaymeric.david@gmail.com>
2020-12-03 17:21:52 +0100

    scroll navbar

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,16 @@
+const initUpdateNavbarOnScroll = () => {
+  const navbar = document.querySelector('.navbar-lewagon');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      // const condition = window.scrollY >= window.innerHeight;
+      const condition = window.scrollY >= window.innerHeight;
+      if (condition) {
+        navbar.classList.add('sticky');
+      } else {
+        navbar.classList.remove('sticky');
+      }
+    });
+  }
+}
+
+export { initUpdateNavbarOnScroll };
