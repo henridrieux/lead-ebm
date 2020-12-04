011b021be2feb2203dbe3b351ca905f27e2e5203
Pierre David <pierreaymeric.david@gmail.com>
2020-12-04 11:34:38 +0100

    locales et dates fr

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module LeadEbm
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    I18n.available_locales = [:en, :fr]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
