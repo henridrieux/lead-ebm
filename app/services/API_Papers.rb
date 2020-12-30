1bf7ccf014f4570b28aaccee762070b159ef7f9b
Henri des Rieux <desrieux.henri@gmail.com>
2020-12-30 18:17:52 +0100

    openuri

--- app/services/API_Papers.rb
+++ app/services/API_Papers.rb
@@ -398,26 +398,38 @@ class APIPapers
   end
 
   def email(siren, category)
-    url = http(siren, category)
-    p url
-    if http(siren, category).nil? || url == nil?
-      email_address = "N.C."
-    elsif
-      begin
-      open(url)
-      rescue OpenURI::HTTPError => error
-      # response = error.io
-      # response.status
-      # => ["503", "Service Unavailable"]
-      end
+    if http(siren, category).nil?
       email_address = "N.C."
     else
+      # url = http(siren, category)
+      # p url
+      # html_file = open(url).read
+      check_url_validity(siren, category)
+
+      return email_address
+    end
+  end
+
+  def check_url_validity(siren, category)
+    url = http(siren, category)
+    p url
+    if remote_file_exist?(url)
       html_file = open(url).read
-      if html_file.nil? || html_file.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i).nil?
-        email_address = "N.C."
-      else
-        email_address = html_file.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i)[0].to_s
-      end
+      check_email_adress(html_file)
+    else
+      email_address = "N.C."
+    end
+  end
+
+  def remote_file_exist?(url)
+    open(url, :method => :head).status rescue false
+  end
+
+  def check_email_adress(html_file)
+    if html_file.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i).nil?
+      email_address = "N.C."
+    else
+      email_address = html_file.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i)[0].to_s
     end
     return email_address
   end
