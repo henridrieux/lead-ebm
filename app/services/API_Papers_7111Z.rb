637adab949f415a15c42bab96bb966a38a2b71e9
Henri des Rieux <desrieux.henri@gmail.com>
2021-01-07 13:58:39 +0100

    test

--- app/services/API_Papers_7111Z.rb
+++ app/services/API_Papers_7111Z.rb
@@ -376,7 +376,7 @@ def papers_all(number, date_string)
   end
 
   def check_email_adress(html_file)
-    if html_file.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i).nil?
+    if html_file.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i).nil? || html_file.match(/[A-Z0-9._%+-]+@sentry.wixpress.com/i)
       email_address = "N.C."
     else
       email_address = html_file.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i)[0].to_s
